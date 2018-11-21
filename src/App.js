365173416da378af99f61612554e1d81d05fccfc
YaroslavW <doctor355@yahoo.com>
2018-11-21 20:50:22 +0300

    Components structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header'
+import About from './Components/About'
+import Resume from './Components/Resume'
+import Portfolio from './Components/Portfolio'
+import Testimonials from './Components/Testimonials'
+import Contact from './Components/Contact'
+import Footer from './Components/Footer'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header />
+        <About />
+        <Resume />
+        <Portfolio />
+        <Testimonials />
+        <Contact />
+        <Footer />
       </div>
     );
   }
